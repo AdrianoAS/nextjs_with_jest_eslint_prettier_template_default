04a8a06a8dc0e369846af73cb2fe506adcaaa2ee
AdrianoAS <aadriano990@gmail.com>
2022-06-17 16:39:40 -0300

    Adicionado configurações no global styled

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,7 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
+
   *{
     margin: 0;
     padding: 0;
@@ -11,6 +12,12 @@ export default createGlobalStyle`
     background: ${props => props.theme.colors.background};
     color: ${props => props.theme.colors.text};
     font: 400 16px Lato, sans-serif;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialized;
+  }
+
+  html, body, #root{
+    height: 100% ;
   }
 
   li{
@@ -19,6 +26,19 @@ export default createGlobalStyle`
 
   a{
     text-decoration: none;
+  }
+
+  button{
+    cursor: pointer;
+  }
+
+  div, a, img{
+    -webkit-tap-highlight-color: rgba(0,0,0,0);
+  }
+
+  img{
+    max-width: 100%;
+    height: 100%;
   }
 
 `
